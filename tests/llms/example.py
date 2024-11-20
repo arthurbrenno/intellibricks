22ed29b1d004f4f006369f490cd2739f4512c192
Arthur Brenno <64020210+arthurbrenno@users.noreply.github.com>
2024-11-20 13:33:44 -0300

    docs: better documentation examples

--- tests/llms/example.py
+++ tests/llms/example.py
@@ -1,10 +1,8 @@
 from dotenv import load_dotenv
 from msgspec import Struct
-
 from intellibricks import CompletionEngine
 
 load_dotenv(override=True)
-
 
 class Joke(Struct):
     joke: str
@@ -12,8 +10,7 @@ class Joke(Struct):
 
 output = CompletionEngine().complete(
     prompt="Tell me a joke",
-)  # Evaluates to CompletionOutput[Joke]
+    response_format=Joke
+)
 
-output.choices
-funny_joke = output.get_parsed()  # Evaluates to Joke
-print(funny_joke)
+print(output.get_parsed()) # Joke obj
