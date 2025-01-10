85819392d38dbe7845d6d7140e6cdb459f67565e
Arthur Brenno <64020210+arthurbrenno@users.noreply.github.com>
2025-01-10 16:43:16 -0300

    wip

--- tests/example.py
+++ tests/example.py
@@ -6,6 +6,9 @@ class Response(msgspec.Struct):
     response: str
 
 
+print(Response.__qualname__)
+exit()
+
 synapse = Synapse.of("google/genai/gemini-1.5-flash")
 
 completion = synapse.complete("Hello, how are you?", response_model=Response)
