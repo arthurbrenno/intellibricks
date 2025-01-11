b3eb92fcab74f8226334d965d7e15f1fb522fa8f
Arthur Brenno <64020210+arthurbrenno@users.noreply.github.com>
2025-01-11 14:46:46 -0300

    wip

--- tests/example.py
+++ tests/example.py
@@ -6,7 +6,7 @@ class Response(msgspec.Struct):
     response: str
 
 
-synapse = Synapse.of("cerebras/api/llama3.1-70b")
+synapse = Synapse.of("google/genai/gemini-2.0-flash-exp")
 
 completion = synapse.complete("Hello, how are you?", response_model=Response)
 print(completion)
